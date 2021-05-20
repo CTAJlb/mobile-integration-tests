1588b6d4cabe97415ea39a8b410fe18aa27239d4
Alex Kravchenko <a2.kravchenko@a1qa.com>
2021-05-20 17:11:05 +0300

    fixed parallel

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,28 +1,27 @@
 package runners;
 
-import courgette.api.CourgetteOptions;
-import courgette.api.CourgetteRunLevel;
 import courgette.api.CucumberOptions;
-import courgette.api.testng.TestNGCourgette;
-import framework.utilities.feedXMLUtil.XMLUtil;
-import org.testng.annotations.Test;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import org.testng.annotations.DataProvider;
 
-@Test
-@CourgetteOptions(
-        threads = 2,
-        runLevel = CourgetteRunLevel.FEATURE,
-        cucumberOptions = @CucumberOptions(
-                features = {"src/test/java/features"},
-                glue = {
-                        "hooks",
-                        "transformers",
-                        "stepdefinitions"
-                },
-                plugin = {
-                        "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm",
-                        //todo Aquality Tracking is temporary turned off until AqualityTrackingCucumber6Jvm is released
-                        //"aquality.tracking.integrations.cucumber5jvm.AqualityTrackingCucumber5Jvm",
-                }
-        ))
-public class TestRunner extends TestNGCourgette {
+@CucumberOptions(
+        features = {"src/test/java/features"},
+        glue = {
+                "hooks",
+                "transformers",
+                "stepdefinitions"
+        },
+        plugin = {
+                "io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm",
+                //todo Aquality Tracking is temporary turned off until AqualityTrackingCucumber6Jvm is released
+                //"aquality.tracking.integrations.cucumber5jvm.AqualityTrackingCucumber5Jvm",
+        }
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true)
+    public Object[][] scenarios() {
+        return super.scenarios();
+    }
 }
