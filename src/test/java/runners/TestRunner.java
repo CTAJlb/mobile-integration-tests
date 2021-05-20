cd723a6895950f92eb487eb43ee52ba177d56b7b
Alex Kravchenko <a2.kravchenko@a1qa.com>
2021-05-20 17:35:07 +0300

    fixed pom

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,7 +1,7 @@
 package runners;
 
-import courgette.api.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
 @CucumberOptions(
