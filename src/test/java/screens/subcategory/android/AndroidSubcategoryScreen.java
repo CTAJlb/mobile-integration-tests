a85e902cfd49372a3358a4c2a23a5507422ca549
Alex Kravchenko <a2.kravchenko@a1qa.com>
2021-07-13 10:56:09 +0300

    fixed for Android3

--- src/test/java/screens/subcategory/android/AndroidSubcategoryScreen.java
+++ src/test/java/screens/subcategory/android/AndroidSubcategoryScreen.java
@@ -33,8 +33,8 @@ public class AndroidSubcategoryScreen extends SubcategoryScreen {
     private static final String TYPE_INFO_XPATH =
             "//android.widget.TextView[contains(@resource-id, \"bookCellIdleMeta\")]";
     public static final String BOOK_BUTTON_WITH_DEFINITE_NAME_AND_DEFINITE_ACTION_BUTTON_LOCATOR_PATTERN = "//android.widget.TextView[contains(@resource-id, \"bookCellIdleTitle\") and @text=\"%s\"]/following-sibling::android.widget.LinearLayout//android.widget.Button[@text=\"%s\"]";
-    public static final String BOOK_TITLE_WITH_DEFINITE_NAME_AND_DEFINITE_ACTION_BUTTON_ENDING_PART_LOCATOR_PATTERN = "/parent::android.widget.LinearLayout/preceding-sibling::android.widget.TextView[contains(@resource-id, \"bookCellIdleTitle\")";
-    public static final String BOOK_WITH_DEFINITE_NAME_LOCATOR_PATTERN = "//android.widget.TextView[contains(@resource-id, \"bookCellIdleTitle\") and @text=\"%s\"]";
+    public static final String BOOK_TITLE_WITH_DEFINITE_NAME_AND_DEFINITE_ACTION_BUTTON_ENDING_PART_LOCATOR_PATTERN = "/parent::android.widget.LinearLayout/preceding-sibling::android.widget.TextView[contains(@resource-id, \"bookCellIdleTitle\")]";
+    public static final String BOOK_TITLE_WITH_DEFINITE_NAME_LOCATOR_PATTERN = "//android.widget.TextView[contains(@resource-id, \"bookCellIdleTitle\") and @text=\"%s\"]";
     public static final String AUTHOR_LABEL_LOCATOR_PART = "/parent::android.widget.LinearLayout/preceding-sibling::android.widget.TextView[contains(@resource-id, \"bookCellIdleAuthor\")]";
 
     private final ILabel lblFirstBookImageCover =
@@ -80,7 +80,7 @@ public class AndroidSubcategoryScreen extends SubcategoryScreen {
     @Override
     public void openBook(CatalogBookModel bookInfo) {
         String bookTitle = bookInfo.getTitle();
-        IButton button = getElementFactory().getButton(By.xpath(String.format(BOOK_WITH_DEFINITE_NAME_LOCATOR_PATTERN, bookTitle)), bookTitle);
+        IButton button = getElementFactory().getButton(By.xpath(String.format(BOOK_TITLE_WITH_DEFINITE_NAME_LOCATOR_PATTERN, bookTitle)), bookTitle);
         if (!button.state().waitForDisplayed()) {
             button.getTouchActions().scrollToElement(SwipeDirection.DOWN);
         }
