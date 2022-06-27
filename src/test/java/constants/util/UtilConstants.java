185cfebf92d4c4752417f3613b5770d748f82c9e
Kate Astapkina <e.astapkina@a1qa.com>
2022-06-27 18:52:12 +0300

    updating tests Catalog navigation

--- src/test/java/constants/util/UtilConstants.java
+++ src/test/java/constants/util/UtilConstants.java
@@ -6,4 +6,5 @@ public class UtilConstants {
     public static final String AVAILABLE = "available";
     public static final String UNAVAILABLE = "unavailable";
     public static final String NEXT = "next";
+    public static final String PALACE_BOOKSHELF = "Palace Bookshelf";
 }
