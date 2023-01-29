455aa887b6f8767d826711dac89cf618d6d79832
Astapkina Ekaterina <astapkina.caterina@gmail.com>
2023-01-29 21:30:19 +0400

    renamed methods

--- src/test/java/enums/localization/translation/Spanish.java
+++ src/test/java/enums/localization/translation/Spanish.java
@@ -44,12 +44,12 @@ public enum Spanish implements LocalizedValue {
     private final String key;
 
     @Override
-    public String i18n() {
+    public String getDefaultLocalizedValue() {
         return localizationProvider.getLocalization(key);
     }
 
     @Override
-    public String i18n(@NonNull Locale locale) {
+    public String getLocalizedValueOfSpecificLocale(@NonNull Locale locale) {
         return localizationProvider.getLocalization(key, locale);
     }
 }
