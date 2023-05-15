dd59bd3b9b1c650eb4fa72ec211fdd8111e6f43e
Astapkina Ekaterina <astapkina.caterina@gmail.com>
2023-05-15 21:16:35 +0400

    updated reserve screen ios

--- src/test/java/screens/audiobook/bookmarksaudiobook/ios/IosBookmarksAudiobookScreen.java
+++ src/test/java/screens/audiobook/bookmarksaudiobook/ios/IosBookmarksAudiobookScreen.java
@@ -1,9 +1,12 @@
 package screens.audiobook.bookmarksaudiobook.ios;
 
+import aquality.appium.mobile.application.PlatformName;
 import aquality.appium.mobile.elements.interfaces.ILabel;
+import aquality.appium.mobile.screens.screenfactory.ScreenType;
 import org.openqa.selenium.By;
 import screens.audiobook.bookmarksaudiobook.BookmarksAudiobookScreen;
 
+@ScreenType(platform = PlatformName.IOS)
 public class IosBookmarksAudiobookScreen extends BookmarksAudiobookScreen {
 
     private final ILabel lblNoBookmarks = getElementFactory().getLabel(By.xpath("//XCUIElementTypeStaticText[contains(@name=\"no bookmarks\")]"), "No bookmarks label");
